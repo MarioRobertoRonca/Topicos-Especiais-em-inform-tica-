3681cca819c666750aeba0fe57f6e0392b244bd0
Mário Roberto <50268227+MarioRobertoRonca@users.noreply.github.com>
2021-03-22 20:48:19 -0300

    First commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../Template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small='Nós'></PageHeader>
+        <h2>Nossa história</h2>
+        <p>Somas a turma mais top</p>
+        <h2>Missão e Visão</h2>
+        <p>Vem com o 6ºADS</p>
+    </div>
+)
